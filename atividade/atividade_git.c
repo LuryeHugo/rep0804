0a830b135376791b481c32fddb1a3eff9ab440b5
LuryeHugo <lury2000@hotmail.com>
2016-04-08 16:18:03 -0300

    Atividade Thiago 08/04

---
+++ atividade/atividade_git.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+ 
+       char aluno1[] = "Lury";
+       char aluno2[] = "Hugo";
+
+       printf("Atividade de Git!\n%s\n%s", Lury, Hugo);
+
+       return 0;
+
+}
